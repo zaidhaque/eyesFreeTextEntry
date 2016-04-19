3debba2e5df9dbbe8e3234943b037fbd4de16342
zaidhaque <zaidhaque@gmail.com>
2016-04-19 18:21:24 -0400

    testing horizontal v. vertical keyboard layout

--- eyesFree/eyesFree/ViewController.swift
+++ eyesFree/eyesFree/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var OutputTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
